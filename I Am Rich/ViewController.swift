bea754f2e40022689f5d196d9c04c5b04bb2c21f
Luis Locon <llocon@cognits.co>
2020-05-12 22:38:22 -0600

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Luis Locón on 5/12/20.
+//  Copyright © 2020 Luis Locón. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
